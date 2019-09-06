80ef0b86ca0b708e9b2a78dee202811d7de29730
fez3d <soydanielmedina@gmail.com>
2019-09-06 13:57:10 -0500

    added work_place field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
+  field :work_place, type: String
+
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
+
+  def full_name
+    "#{name} #{last_name}"
+  end
 end
